37502ead5e6092545ad5cd413cf256775d7a8bad
Desmond <desmondowq@gmail.com>
2015-03-12 00:38:09 +0800

    skrollr

--- scripts/custom.js
+++ scripts/custom.js
@@ -3,10 +3,13 @@
  */
 function parallaxContainer(){
     var bodyWidth = $(window).width();
-    if(bodyWidth <= 768){
-        bodyWidth = 768;
+    if(bodyWidth < 450){
+        bodyWidth = 450;
     }
-    var parallaxHeight = Math.round(bodyWidth * 0.5625);
+    //if(bodyWidth <= 768){
+    //    bodyWidth = 768;
+    //}
+    //var parallaxHeight = Math.round(bodyWidth * 0.5625);
     //console.log(parallaxHeight);
     //$('.parallax_field').css('height',parallaxHeight+"px");
     //if(parallaxHeight > 432){
@@ -14,6 +17,9 @@ function parallaxContainer(){
     //}else{
     //    $('.intro_video_context').css('top','20%');
     //}
+    if($('#fit_in_video').length){
+        $('#fit_in_video').css('height',(bodyWidth*0.55)*0.5625);
+    }
 }
 $(window).resize(function() {
     parallaxContainer();
