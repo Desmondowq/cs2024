d75a1d338295e9c0a03ca586dd2b030048f983ac
Zhong Huiwen <zhonghuiwen@yahoo.com.sg>
2015-03-12 05:11:04 +0800

    redesigned index, changed videos, fixed app section on index, added multiple videos on index, hide menu until index is scrolled down, added a bouncing arrow to tell user to scroll down, added playVideo() in custom.js to detect when to play video based on user's scroll position

--- scripts/custom.js
+++ scripts/custom.js
@@ -24,3 +24,31 @@ function parallaxContainer(){
 $(window).resize(function() {
     parallaxContainer();
 });
+
+/*http://stackoverflow.com/questions/19674315/how-to-stop-html5-video-from-playing-when-out-of-viewport*/
+function playVideo($video, desiredPositionStart, currentPosition){
+    var videoElem = $video[0];
+
+    var videoTop = $video.offset().top;
+    var videoBottom = videoTop + $video.height();
+
+    var viewTop = $(window).scrollTop();
+    var viewBottom = viewTop + $(window).height();
+
+//play video when opacity is at desiredPosition and at least some of the video player is show
+    var isViewable = currentPosition > desiredPositionStart && (videoTop < viewBottom && videoBottom > viewTop);
+    if(isViewable) {
+        if(videoElem.paused) {
+            videoElem.play();
+            console.log("play");
+        }
+    }
+    else {
+        if(!videoElem.paused) {
+            videoElem.pause();
+            videoElem.currentTime = 0;
+            videoElem.load();
+            console.log("pause");
+        }
+    }
+}
